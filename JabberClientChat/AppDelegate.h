1d38333cf8832037b15bb69e1faee9b8553f2ba5
rushisangani <rushisangani@gmail.com>
2015-09-08 18:03:10 +0530

    Initial Commit

---
+++ JabberClientChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JabberClientChat
+//
+//  Created by Rushi Sangani on 08/09/15.
+//  Copyright (c) 2015 Codal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JabberClientChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JabberClientChat
+//
+//  Created by Rushi Sangani on 08/09/15.
+//  Copyright (c) 2015 Codal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
